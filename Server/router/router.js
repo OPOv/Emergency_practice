657246245b6e71a4d63809d9408b8d3a94d72e0f
lolhi <lolhi@naver.com>
2018-09-23 17:41:00 +0900

    Node.js Server 추가

---
+++ Server/router/router.js
@@ -0,0 +1,42 @@
+module.exports = function(app, fs)
+{
+  app.get('/HospitalData', function(req,res){
+    fs.readFile(__dirname + "/../data/HospitalData.json",'utf8',function(err,data){
+      var jsondata = JSON.parse(data);
+
+      var length = Object.keys(jsondata.DATA).length;
+
+      var i;
+      var jsonStr = "[";
+      for(i = 0; i < length; i++){
+        if(jsondata.DATA[i].MC_TYPE == "의원" || jsondata.DATA[i].MC_TYPE == "종합병원"
+        || jsondata.DATA[i].MC_TYPE == "병의원" || jsondata.DATA[i].MC_TYPE == "병원"){
+          var temp = JSON.stringify(jsondata.DATA[i]);
+
+          if(i != 0)
+            jsonStr = jsonStr + ",";
+          jsonStr = jsonStr + temp;
+        }//end if
+      }//end for loop
+      jsonStr = jsonStr + "]";
+      jsonStr = JSON.parse(jsonStr);
+      res.status(200).json(jsonStr);
+    })//end readFile()
+  })//end app.get()
+
+  app.get('/ShelterData', function(req,res){
+    fs.readFile(__dirname + "/../data/ShelterData.json",'utf8',function(err,data){
+      var jsondata = JSON.parse(data);
+
+      res.status(200).json(jsondata.DATA);
+    })//end readFile()
+  })//end app.get()
+
+  app.get('/KnowledgeData', function(req,res){
+    fs.readFile(__dirname + "/../data/KnowledgeData.json",'utf8',function(err,data){
+      var jsondata = JSON.parse(data);
+
+      res.status(200).json(jsondata.DATA);
+    })//end readFile()
+  })//end app.get()
+}
